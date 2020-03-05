39240c213b8b368e3fadcf6f5395637edb9f8f54
YouyuZhao <1213366162@qq.com>
2020-03-05 22:04:40 +0800

    banner样式完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
+//渲染nav
 getList();
 function getList(){
     $.ajax({
         url:"../lib/json/nav.json",
         dataType:"json",
         success:function(res){
-            console.log(res)
+            // console.log(res)
 
             //准备一个空字符串，准备渲染一级菜单
             let str="";
@@ -26,7 +27,7 @@ function getList(){
                 .on("mouseover",function(){
                     const index=$(this).index();
                     const list=res[index].list;//找到要渲染的list数组
-                    console.log(list)
+                    // console.log(list)
 
                     //准备一个空字符串，准备渲染二级菜单
                     let str="";
@@ -58,6 +59,90 @@ function getList(){
                 })
             
             
+        }
+    })
+}
+$(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        // direction: 'vertical', // 垂直切换选项
+        loop: true, // 循环模式选项
+        autoplay:{
+            delay:1000},
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // 如果需要滚动条
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    }) 
+   })
+
+//渲染banner下面的nav-list
+getNavlist();
+function getNavlist(){
+    $.ajax({
+        url:"../lib/json/banner.json",
+        dataType:"json",
+        success:function(res){
+            console.log(res);
+
+            //准备一个空字符串
+            let str="";
+
+            //开始渲染nav-list
+            res.forEach(function(item){
+              str+=`
+              <li>
+              <span>${item.title}</span>
+              <i class="iconfont icon-arrow-right-copy-copy-copy"></i>
+              </li>`  
+            })
+
+            //填充到nav-list的ul里面
+            $(".banner .nav-list ul")
+                .html(str)
+            //并给它添加移入移出事件
+                .on({
+                    mouseenter:function(){$(".nav-list-box").stop().show()},
+                    mouseleave:function(){$(".nav-list-box").stop().hide()}
+                })
+            
+            //找到nav-list的ul里面的所有的li
+               .children("li")
+               .on("mouseover",function(){
+                   $(this).css('background', 'red');//移入时颜色变红
+                   const index=$(this).index();
+                   const list=res[index].list;//找到要渲染的list数组
+                //    console.log(list)
+
+                //准备一个空字符串准备渲染nav-list-box
+                   let str="";
+                   //开始渲染
+                   list.forEach(function(item){
+                        str+=`<li>${item.name}</li>`
+                   })
+                   //放到.nav-list-box的ul里面
+                   $(".nav-list-box ul")
+                        .html(str)
+               })
+               .on("mouseout",function(){
+                   $(this).css('background', '');//移出时颜色复原
+               })
+
+               $(".nav-list-box")
+                    .on({
+                        mouseover:function(){$(this).finish().show()},
+                        mouseout:function(){$(this).finish().hide()}
+                    })
         }
     })
 }
